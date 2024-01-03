3987e0f345e16d30b7a66a95c68e289c933c7568
Jayant Work <122964204+jayantkoolio@users.noreply.github.com>
2024-01-03 22:12:19 +0530

    Update audiogenforecs.py

--- audiogenforecs.py
+++ audiogenforecs.py
@@ -8,13 +8,25 @@ from audiocraft.models import AudioGen
 from dotenv import load_dotenv
 import sys
 
-model = AudioGen.get_pretrained('facebook/audiogen-medium')
+model = AudioGen.get_pretrained('./huggingface/hub/models--facebook--audiogen-medium/snapshots/389bbdcd572a583ebf9e120b1a0f9f809eb005a2/')
+
+AWS_SERVER_PUBLIC_KEY = os.environ["ACCESS_KEY"]
+AWS_SERVER_SECRET_KEY = os.environ["SECRET_KEY"]
 
 load_dotenv()
 RECEIPT_HANDLE = os.environ.get("RECEIPT_HANDLE")
 QUEUE_URL = os.environ.get("QUEUE_URL")
 
-s3 = boto3.resource('s3')
+# s3 = boto3.resource('s3')
+# s3 = boto3.resource("s3", aws_access_key_id=ACCESS_KEY, aws_secret_access_key=SECRET_KEY)
+
+session = boto3.Session(
+    aws_access_key_id=AWS_SERVER_PUBLIC_KEY,
+    aws_secret_access_key=AWS_SERVER_SECRET_KEY,
+)
+
+s3 = session.resource("s3")
+
 BUCKET = "ns.riffusion.test"
 FOLDER = "dev_audiogen/"
 FOLDER_QA = "qa_audiogen/"
@@ -48,13 +60,13 @@ def create_audio(prompt, filename, username, env, length, jobnam):
     sqs_client.delete_message(QueueUrl=QUEUE_URL, ReceiptHandle=RECEIPT_HANDLE)
 
 data = os.environ.get("DATA")
-data = json.loads(data)
 print(data)
+# data = '{"positive_text": "cat meowing", "username": "jayant1_lambda", "env" : "development", "jobname": "ecefb906-27c5-4024-a83f-fb689dfd7365", "duration": "5.0" }'
+data = json.loads(data)
 prompt = data['positive_text']
 username = data['username']
 ENV = data['env']
 job = data['jobname']
 FILENAME =  prompt + "_audiogen" +  ".wav"
-duration = data['duration']
+duration = float(data['duration'])
 create_audio(prompt, FILENAME, username, ENV, duration, job)
-return "Done"
